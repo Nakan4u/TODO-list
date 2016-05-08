dd6ba01818d8e9bc3b33b3410e5d8178de774f3e
Grigoriy Nakonechnyi <nakan88@gmail.com>
2016-05-08 21:20:02 +0300

    implement add form functionality

--- app_angular/js/controllers/main.js
+++ app_angular/js/controllers/main.js
@@ -1,17 +1,32 @@
 var app = angular.module('myApp');
 
 app.controller('mainCtrl', function ($scope) {
+    
     $scope.callList = [
         {
             id: 1,
             name: 'Sam Tailor',
             phone: '2189-3298',
-            time: '13:38'
-        }, {
-            id: 2,
-            name: 'John Smith',
-            phone: '4372-23',
-            time: '17:23'
+            time: 1288323623006
         }
     ];
+    $scope.render = function() {
+        var localData = localStorage.getItem('list');
+        if (localData) {
+            $scope.callList = JSON.parse(localData);
+        }
+    }
+    $scope.saveData = function(entry) {
+        var id = $scope.callList.length + 1;
+        
+        entry.id = id;
+        $scope.callList.push(entry);
+        $scope.updateData();
+    }
+    $scope.updateData = function() {
+        localStorage.setItem('list', JSON.stringify($scope.callList));
+    }
+    
+    // init app
+    $scope.render();
 });
--- app_angular/js/directives/addCall/script.js
+++ app_angular/js/directives/addCall/script.js
@@ -4,26 +4,51 @@ app.directive('addCallForm', function () {
         templateUrl: './js/directives/addCall/tpl.html',
         link: function ($scope) {
             $scope.name = '';
-            $scope.phone = '911';
+            $scope.nameMaxLength = 30;
+            $scope.phone = '+(420) 111 222 333';
+            $scope.phonePattern = '(\\+|00)(\\(?\\d{3}\\)?-?)(\\s?\\d{3}){3}';
             $scope.time = '10:22';
-            
-            $scope.addCall = function() {
+            $scope.timePattern = '([01]\\d|2[0-3]):?([0-5]\\d)';
+
+            $scope.addCall = function () {
                 var newCall = {};
-                
+
                 if ($scope.name && $scope.phone && $scope.time) {
+                    
+                    $scope.phone = $scope.convertPhone($scope.phone);
+                    
                     newCall = {
                         name: $scope.name,
                         phone: $scope.phone,
                         time: $scope.time
                     }
-                    $scope.callList.push(newCall);
+                    //$scope.callList.push(newCall);
+                    $scope.saveData(newCall);
                     $scope.reset();
+                    
                 }
             }
-            $scope.reset = function() {
+            
+            $scope.reset = function () {
+                //reset fields data
                 $scope.name = '';
                 $scope.phone = '';
                 $scope.time = '';
+                
+                //reset form validation
+                $scope.addCallForm.$setPristine();
+                $scope.addCallForm.$setValidity();
+                $scope.addCallForm.$setUntouched();
+            }
+            
+            $scope.convertPhone = function (phone_number) {
+                var result = phone_number;
+                
+                //convert to this line 00XXX XXX XXX XXX
+                result = result.replace(/\(|\)|-| /g, "");
+                result = result.replace(/\+/g, "00");
+                
+                return result.slice(0, 5) + ' ' + result.slice(5, 8) + ' ' + result.slice(8, 11) + ' ' + result.slice(11);
             }
         }
     }
