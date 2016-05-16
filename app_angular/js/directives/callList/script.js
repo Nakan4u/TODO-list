98cc415ad2f567be5a496fb6c02ed77cf9e54cd0
Grigoriy Nakonechnyi <nakan88@gmail.com>
2016-05-17 00:13:46 +0300

    add timeViewFilter for buttons in call list

--- app_angular/js/directives/callList/script.js
+++ app_angular/js/directives/callList/script.js
@@ -12,10 +12,27 @@ app.directive('callList', function () {
             };
             
             $scope.timeInPast = function (time) {
-                
                 var currentTime = new Date().getTime();
 
                 return (time <= currentTime) ? true : false;
+            }
+            
+            // timeViewFilter
+            $scope.timeFilter = 'all'; // also can be future or past
+            $scope.setTimeFilter = function (value) {
+                $scope.timeFilter = value;
+            };
+            $scope.toogleView = function (time) {
+                var currentTime = new Date().getTime(),
+                    timeFilter = $scope.timeFilter;
+                
+                if ((timeFilter === 'finished' && time >= currentTime) || // for finished filter we hide future items
+                    (timeFilter === 'next' && time < currentTime)) {  // for next filter we will do revert
+                        
+                    return false;
+                } else {
+                    return true; // for all we show all items
+                }
             }
         }
     }
