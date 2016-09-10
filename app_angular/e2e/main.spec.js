8b11c41fa4a896eaf40c4c6a67f329dd97a78b9f
Grigoriy Nakonechnyi <nakan88@gmail.com>
2016-09-10 18:04:47 +0300

    step-2

--- app_angular/e2e/main.spec.js
+++ app_angular/e2e/main.spec.js
@@ -15,4 +15,18 @@ describe('ToDoCalls App', function () {
         expect(browser.getTitle()).toEqual('ToDoCalls angular app');
     });
 
+    describe('Add call form', function () {
+
+        it('should show error when attempt submit with empty name field', function () {
+            var nameField = element(by.model('name')),
+                submitButton = element(by.css('#add-button')),
+                nameRequired = element(by.css('span[ng-show="addCallForm.userName.$error.required"]'));
+
+            nameField.sendKeys('');
+            submitButton.click();
+
+            expect(nameRequired.isDisplayed()).toBeTruthy();
+        });
+    });
+
 });
