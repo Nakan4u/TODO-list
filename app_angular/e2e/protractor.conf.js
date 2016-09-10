f2cfa0446f46ebf15a08cb6ee5f087f09ca3002f
Grigoriy Nakonechnyi <nakan88@gmail.com>
2016-09-10 22:18:42 +0300

    step-7, add responsive config;

--- app_angular/e2e/protractor.conf.js
+++ app_angular/e2e/protractor.conf.js
@@ -3,8 +3,8 @@ exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:8000/',
-  specs: ['main.spec.js'],
-  suites: { // to run suite : protractor --suite callList e2e/protractor.conf.js
+  specs: ['main.spec.js'], // to run single test : protractor e2e/protractor.conf.js --spec callList.spec.js
+  suites: { // to run suite : protractor e2e/protractor.conf.js --suite callList
     main: 'main.spec.js',
     callList: 'callList.spec.js'
   },
@@ -27,5 +27,42 @@ exports.config = {
       phone: '911',
       time: 'abc'
     }
-  }
+  },
+  multiCapabilities: [
+    // Desktop
+    {
+      'browserName': 'chrome',
+      'logName': 'Desktop',
+      'chromeOptions': {
+        args: [
+          '--lang=en',
+          '--window-size=1024,800'
+        ]
+      }
+    },
+
+    // Tablet
+    {
+       'browserName': 'chrome',
+       'logName': 'Tablet',
+       'chromeOptions' : {
+            args: [
+                '--lang=en',
+                '--window-size=768,1024'
+            ]
+        }
+    },
+
+    // Mobile
+    {
+       'browserName': 'chrome',
+       'logName': 'Mobile',
+       'chromeOptions' : {
+            args: [
+                '--lang=en',
+                '--window-size=320,480'
+            ]
+        }
+    }
+  ],
 }
